11af9d39c27ab5565c748d8acb4b8e9b4430b469
Ilya <ilya.borisov.bisec@gmail.com>
2023-06-14 12:35:07 +0300

    9.01

---
+++ RunExercise/RunExercise/9/9_1.cpp
@@ -0,0 +1,13 @@
+#include "9_1.h"
+
+int main()
+{
+	
+
+	return 0;
+}
+
+
+
+
+
--- RunExercise/RunExercise/9/9_1.h
+++ RunExercise/RunExercise/9/9_1.h
@@ -1,1 +1,35 @@
 #pragma once
+
+#include "..\stdafx.h"
+
+const int LENGTH = 80;
+
+class Publication
+{
+protected:
+	char mTittle[LENGTH];
+	float price;	
+public:
+	void getData();
+	void putData();
+};
+
+class Book :public Publication
+{
+private:
+	int mCountPage; // count of page
+
+public:
+	void getData();
+	void putData();
+};
+
+class Type : public Book
+{
+private:
+	float mRecordTime;	// recording time in minutes
+public:
+	void getData();
+	void putData();
+};
+
