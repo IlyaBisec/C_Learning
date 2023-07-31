51a38cc63af3c1fd758eee5a9e67edfc5dfd6182
Ilya <ilya.borisov.bisec@gmail.com>
2023-07-31 18:28:24 +0300

    Update 10_4.h

--- RunExercise/RunExercise/10/10_4.h
+++ RunExercise/RunExercise/10/10_4.h
@@ -1,6 +1,27 @@
+// List
+
 #pragma once
+#include "..\stdafx.h"
+using namespace std;
 
 namespace TenFour
 {
-	
+	struct Link // Element of linklist
+	{
+		int data;   // Data
+		Link* next; // Pointer on the next element of linklist
+	};
+
+	class LinkList
+	{
+	public:
+		LinkList() { m_First = NULL; }
+		~LinkList(){}
+
+		void addItem(int data);
+		void display();
+
+	private:
+		Link* m_First;
+	};
 }
