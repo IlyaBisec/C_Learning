904565b318c6c2db7aad31b613ff5a4a171ecddb
Ilya-BISEC <ilya.borisov.bisec@gmail.com>
2024-03-19 14:29:19 +0300

    add const&, change name of argument

--- RunExercise/RunExercise/11/11_2.h
+++ RunExercise/RunExercise/11/11_2.h
@@ -11,23 +11,23 @@ namespace chapter_11 {
 	public:
 		Distance(): m_feet(0),m_inches(0.0){}
 		// Converting float to distance
-		Distance(float temp_feet)
+		Distance(float floatNumber)
 		{
-			m_feet = static_cast<int>(temp_feet);
-			temp_feet, m_inches = 12 * (temp_feet - m_feet);
+			m_feet = static_cast<int>(floatNumber);
+			m_inches = 12 * (floatNumber - m_feet);
 		}
 		Distance(int ft, float inch):m_feet(ft),m_inches(inch){}
 
 		void showDist() const { std::cout << m_feet << "-" << m_inches; }
 		
-		friend float operator*(Distance, Distance); // Prototype
+		friend float operator*(const Distance &, const Distance &); // Prototype
 
 	private:
 		int m_feet;
 		float m_inches;
 	};
 
-	float operator*(Distance dis1, Distance dis2)
+	float operator*(const Distance &dis1, const Distance &dis2)
 	{
 		return (dis1.m_feet + dis1.m_inches / 12) * (dis2.m_feet + dis2.m_inches / 12);
 	}
