f859aca54e69d9c7b30405eb0fdf9e50d012aa6d
Ilya-BISEC <ilya.borisov.bisec@gmail.com>
2024-03-19 14:33:35 +0300

    Formatting the code, add namespace

--- RunExercise/RunExercise/11/11_10.cpp
+++ RunExercise/RunExercise/11/11_10.cpp
@@ -13,7 +13,7 @@ int main11_10()
 		std::cout << "\nInput expression: ";
 		std::cin.get(string, LENGTH);  
 
-		Express *ptrExpression = new Express(string);
+		chapter11_10::Express *ptrExpression = new chapter11_10::Express(string);
 
 		ptrExpression->parse();
 
@@ -28,121 +28,120 @@ int main11_10()
 	return 0;
 }
 
-void Express::parse()
-{
-	char choice;
-	Token *last_value, *last_sign;
+namespace chapter11_10{
 
-	float unsign;
-	Number *ptrNumber;
-	Operator *ptrOperator;
-
-	char *ptrCurrent = m_ptrString;
-
-	while (ptrCurrent[0])
+	void Express::parse()
 	{
-		choice = ptrCurrent[0];
-		
-		// If digits
-		if (choice >= '0' && choice <= '9')
-		{
-			unsign = std::strtof(ptrCurrent, &ptrCurrent);
-			ptrNumber = new Number(unsign);
-			m_Stack.push(ptrNumber);
-		}
-		else if (choice == '+' || choice == '-' || choice == '*'
-			|| choice == '/')
-		{
-			if (m_Stack.gettop() == 1)
-			{
-				ptrOperator = new Operator(choice);
-				m_Stack.push(ptrOperator);
-			}
-			else
-			{
-				last_value = m_Stack.pop();
-				last_sign = m_Stack.pop();
+		char choice;
+		Token *last_value, *last_sign;
 
-				if ((choice == '*' || choice == '/') &&
-					(last_sign->getOperator() == '+' || last_sign->getOperator() == '-'))
+		float unsign;
+		Number *ptrNumber;
+		Operator *ptrOperator;
+
+		char *ptrCurrent = m_ptrString;
+
+		while (ptrCurrent[0])
+		{
+			choice = ptrCurrent[0];
+
+			// If digits
+			if (choice >= '0' && choice <= '9')
+			{
+				unsign = std::strtof(ptrCurrent, &ptrCurrent);
+				ptrNumber = new Number(unsign);
+				m_Stack.push(ptrNumber);
+			} 
+			else if (choice == '+' || choice == '-' || choice == '*' || choice == '/')
+			{
+				if (m_Stack.gettop() == 1)
 				{
-					m_Stack.push(last_sign);
-					m_Stack.push(last_value);
-				}
-				else
+					ptrOperator = new Operator(choice);
+					m_Stack.push(ptrOperator);
+				} else
 				{
-					switch (last_sign->getOperator())
+					last_value = m_Stack.pop();
+					last_sign = m_Stack.pop();
+
+					if ((choice == '*' || choice == '/') &&
+						(last_sign->getOperator() == '+' || last_sign->getOperator() == '-'))
 					{
-					case '+':
-						unsign = m_Stack.pop()->getNumber() + last_value->getNumber();
-						ptrNumber = new Number(unsign);
-						m_Stack.push(ptrNumber);
-						break;
-					case '-':
-						unsign = m_Stack.pop()->getNumber() - last_value->getNumber();
-						ptrNumber = new Number(unsign);
-						m_Stack.push(ptrNumber);
-						break;
-					case '*':
-						unsign = m_Stack.pop()->getNumber() * last_value->getNumber();
-						ptrNumber = new Number(unsign);
-						m_Stack.push(ptrNumber);
-						break;
-					case '/':
-						unsign = m_Stack.pop()->getNumber() / last_value->getNumber();
-						ptrNumber = new Number(unsign);
-						m_Stack.push(ptrNumber);
-						break;
-					default: std::cout << "\nWtf sign\n"; exit(1);
+						m_Stack.push(last_sign);
+						m_Stack.push(last_value);
+					} else
+					{
+						switch (last_sign->getOperator())
+						{
+							case '+':
+								unsign = m_Stack.pop()->getNumber() + last_value->getNumber();
+								ptrNumber = new Number(unsign);
+								m_Stack.push(ptrNumber);
+								break;
+							case '-':
+								unsign = m_Stack.pop()->getNumber() - last_value->getNumber();
+								ptrNumber = new Number(unsign);
+								m_Stack.push(ptrNumber);
+								break;
+							case '*':
+								unsign = m_Stack.pop()->getNumber() * last_value->getNumber();
+								ptrNumber = new Number(unsign);
+								m_Stack.push(ptrNumber);
+								break;
+							case '/':
+								unsign = m_Stack.pop()->getNumber() / last_value->getNumber();
+								ptrNumber = new Number(unsign);
+								m_Stack.push(ptrNumber);
+								break;
+							default: std::cout << "\nWtf sign\n"; exit(1);
+						}
 					}
+					Operator *pOperator = new Operator(choice);
+					m_Stack.push(pOperator);
 				}
-				Operator *pOperator = new Operator(choice);
-				m_Stack.push(pOperator);
+				ptrCurrent++;
+			} else
+			{
+				std::cout << "\nWtf arithmetic sign\n"; exit(1);
 			}
-			ptrCurrent++;
 		}
-		else
+	}
+
+	float Express::solve()
+	{
+		Token *value;
+
+		float unsign;
+		Number *ptrNumber;
+
+		while (m_Stack.gettop() > 1)
 		{
-			std::cout << "\nWtf arithmetic sign\n"; exit(1);
+			value = m_Stack.pop();
+			switch (m_Stack.pop()->getOperator())
+			{
+				case '+':
+					unsign = m_Stack.pop()->getNumber() + value->getNumber();
+					ptrNumber = new Number(unsign);
+					m_Stack.push(ptrNumber);
+					break;
+				case '-':
+					unsign = m_Stack.pop()->getNumber() - value->getNumber();
+					ptrNumber = new Number(unsign);
+					m_Stack.push(ptrNumber);
+					break;
+				case '*':
+					unsign = m_Stack.pop()->getNumber() * value->getNumber();
+					ptrNumber = new Number(unsign);
+					m_Stack.push(ptrNumber);
+					break;
+				case '/':
+					unsign = m_Stack.pop()->getNumber() / value->getNumber();
+					ptrNumber = new Number(unsign);
+					m_Stack.push(ptrNumber);
+					break;
+				default: std::cout << "\nnWtf sign\n"; exit(1);
+			}
 		}
+
+		return m_Stack.pop()->getNumber(); // в стеке осталс€ результат вычислени€ выражени€,
 	}
-}
-
-float Express::solve()
-{
-	Token *value; 
-
-	float unsign;   
-	Number *ptrNumber;
-
-	while (m_Stack.gettop() > 1) 
-	{                     
-		value = m_Stack.pop(); 
-		switch (m_Stack.pop()->getOperator()) 
-		{                  
-		case '+':
-			unsign = m_Stack.pop()->getNumber() + value->getNumber();
-			ptrNumber = new Number(unsign);
-			m_Stack.push(ptrNumber);
-			break;
-		case '-':
-			unsign = m_Stack.pop()->getNumber() - value->getNumber();
-			ptrNumber = new Number(unsign);
-			m_Stack.push(ptrNumber);
-			break;
-		case '*':
-			unsign = m_Stack.pop()->getNumber() * value->getNumber();
-			ptrNumber = new Number(unsign);
-			m_Stack.push(ptrNumber);
-			break;
-		case '/':
-			unsign = m_Stack.pop()->getNumber() / value->getNumber();
-			ptrNumber = new Number(unsign);
-			m_Stack.push(ptrNumber);
-			break;
-		default: std::cout << "\nnWtf sign\n"; exit(1);
-		}
-	}
-
-	return m_Stack.pop()->getNumber(); // в стеке осталс€ результат вычислени€ выражени€,
 }
