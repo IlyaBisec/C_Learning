3a98daf075ff385a02538a7e3983a91517c187a2
Ilya-BISEC <ilya.borisov.bisec@gmail.com>
2024-08-20 00:48:15 +0300

    minor(13_3app.h): english comments

--- RunExercise/RunExercise/13/13_3app.h
+++ RunExercise/RunExercise/13/13_3app.h
@@ -1,9 +1,9 @@
-// Характеристики конкретного здания
+// Characteristics of a specific building
 #pragma once
 
 #ifndef _ELEVATOR_APP_MOD_H_
 #define _ELEVATOR_APP_MOD_H_
 #endif // !_ELEVATOR_APP_MOD_H_
 
-const int COUNT_FLOORS = 20; // количество этажей
-const int COUNT_CABINES = 4; // количество лифтов
+const int COUNT_FLOORS = 20; // Number of floors
+const int COUNT_CABINES = 4; // Number of elevators
