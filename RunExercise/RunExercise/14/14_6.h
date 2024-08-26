c792e8056eeacc40d437e558ed5a65445ce465d6
Sadochok(BISEC) <bisec.world@gmail.com>
2024-08-27 01:39:09 +0300

    add(14_6h): add 14_6 h

---
+++ RunExercise/RunExercise/14/14_6.h
@@ -0,0 +1,30 @@
+// SafeArray with template, because put any types
+#ifndef _SAFEARRAY_TEMPLATE_H_
+#define _SAFEARRAY_TEMPLATE_H_
+#endif // !_SAFEARRAY_TEMPLATE_H_
+#pragma once
+
+#include <iostream>
+
+const int SIZE = 3;
+
+namespace Template_14_6
+{
+	template <class S>
+	class SafeArray
+	{
+	public:
+		S &operator[] (int index)
+		{
+			if (index < 0 || index >= SIZE)
+			{
+				std::cout << "Error index\n";
+				exit(1);
+			}
+			return m_array[index];
+		}
+
+	private:
+		S m_array[SIZE];
+	};
+}
