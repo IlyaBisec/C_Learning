4b03fc7f3bec5d2429c76e98ec3851eb2a7c72d1
Sadochok(BISEC) <bisec.world@gmail.com>
2024-08-27 01:37:19 +0300

    add(14_4): add 14_4 h

---
+++ RunExercise/RunExercise/14/14_4.h
@@ -0,0 +1,19 @@
+ // The swap() function for swap values of aeguments
+// with help template
+#pragma once
+#ifndef _SWAP_TEMPLATE_H_
+#define _SWAP_TEMPLATE_H_
+#endif // !_SWAP_TEMPLATE_H_
+
+#include <iostream>
+
+namespace Template_14_4 {
+    template <class S>
+    void swap(S &argument1, S &argument2)
+    {
+        S temp = argument1;
+        argument1 = argument2;
+        argument2 = temp;
+    }
+}
+
