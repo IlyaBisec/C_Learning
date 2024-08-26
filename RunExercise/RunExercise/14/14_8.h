ba9e2b7f241e278ca3a2f74fc76b1e59012a2f84
Sadochok(BISEC) <bisec.world@gmail.com>
2024-08-27 01:38:20 +0300

    add(14_8): add 14_8 cpp

---
+++ RunExercise/RunExercise/14/14_8.h
@@ -0,0 +1,32 @@
+// Generate index out exclusion class
+#ifndef _ARROVER_EXCLUSION_TEMPLATE_H_
+#define _ARROVER_EXCLUSION_TEMPLATE_H_
+#endif // !_ARROVER_ENCLUSION_TEMPLATE_H_
+#pragma once
+
+#include <iostream>
+
+const int SIZE = 3;
+
+namespace Template_14_8
+{
+	class SafeArray
+	{
+	public:
+		class IndexOut{};
+
+		int &operator[](int index)
+		{
+			if (index < 0 || index >= SIZE)
+			{
+				throw IndexOut();
+			}
+			return m_array[index];
+		}
+	private:
+		int m_array[SIZE];
+	};
+}
+
+
+
