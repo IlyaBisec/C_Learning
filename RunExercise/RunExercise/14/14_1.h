9eee34b38ae6dbc30b7b833055f576c418dccb3e
Sadochok(BISEC) <bisec.world@gmail.com>
2024-08-27 01:35:07 +0300

    add(14_1h): add 14_1 h

---
+++ RunExercise/RunExercise/14/14_1.h
@@ -0,0 +1,25 @@
+// Arithmetical tamplate which return average all 
+// of the arithmetical elements array
+
+#pragma once
+#ifndef _ARITHMETICAL_TEMPLATE_H_
+#define _ARITHMETICAL_TEMPLATE_H_
+#endif
+
+
+#include <iostream>
+
+namespace Template_14_1{
+	template <class T>
+	T average(T * array, int size)
+	{
+		double sum = 0;
+
+		for (int i = 0; i < size; i++)
+		{
+			sum += array[i];
+		}
+
+		return sum / size;
+	}
+}
