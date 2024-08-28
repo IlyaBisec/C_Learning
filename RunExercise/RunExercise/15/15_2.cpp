3368b72a754d47407e43236b7a2fc3033074f22c
Sadochok(BISEC) <bisec.world@gmail.com>
2024-08-29 02:45:04 +0300

    add(15_2): add 15_2 cpp

---
+++ RunExercise/RunExercise/15/15_2.cpp
@@ -0,0 +1,30 @@
+// јлгоритм sort() дл€ сортировки массива слов 
+// push back() - дл€ добавлени€ слов
+// операторы[] and size() - дл€ вывода
+
+#include "header.h"
+
+
+int main15_2()
+{
+	std::vector<std::string> words;
+	std::string word;
+	std::string string = "word";
+
+	char answer;
+
+	getUserData(string, word, words, answer);
+
+	// Enter sort algorithm(to up) to float array digits
+	std::sort(words.begin(), words.end());
+
+	// Output
+	std::cout << "Sorted: ";
+	for (int i = 0; i < words.size(); i++)
+	{
+		std::cout << "\n" << words[i];
+	}
+	std::cout << "\n";
+
+	return 0;
+}
