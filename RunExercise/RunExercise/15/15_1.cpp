68cae9e731c4ba406480936dc1431735fea1ece7
Sadochok(BISEC) <bisec.world@gmail.com>
2024-08-29 02:44:48 +0300

    add(15_1): add 15_1 cpp

---
+++ RunExercise/RunExercise/15/15_1.cpp
@@ -0,0 +1,38 @@
+// Сортировака float массива с помощью sort() STL
+
+#include "header.h"
+
+int main15_1()
+{
+	float array_digits[MAX_COUNT];
+	int index;
+	char answer;
+
+	index = 0;
+
+	do
+	{
+		std::cout << "Enter float number: ";
+		std::cin >> array_digits[index++];
+		
+		if (index < MAX_COUNT)
+		{
+			std::cout << "And more? (y/n): ";
+			std::cin >> answer;
+		}
+
+	} while (answer != 'n' && index < MAX_COUNT);
+
+	// Enter sort algorithm(to up) to float array digits
+	std::sort(array_digits, array_digits + index);
+
+	// Output
+	std::cout << "Sorted: ";
+	for (int i = 0; i < index; i++)
+	{
+		std::cout << array_digits[i] << ' ';
+	}
+	std::cout << "\n";
+
+	return 0;
+}
