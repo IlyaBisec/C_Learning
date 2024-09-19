648afc32c1fd68268576695de7abdd77a2c6d017
Sadochok(BISEC) <bisec.world@gmail.com>
2024-09-20 01:13:13 +0300

    Update 13_1.h

--- RunExercise/RunExercise/13/13_1.h
+++ RunExercise/RunExercise/13/13_1.h
@@ -3,75 +3,69 @@
 #pragma once
 #ifndef _VERYLONG_H_
 #define _VERYLONG_H_
+#define _CRT_SECURE_NO_WARNINGS
 
 #include <iostream>
-#include <string.h>
-#include <stdlib.h>
 
-constexpr int SIZE = 1000; // Max number of digits
+using namespace std;
+
+const int SIZE = 1000; // Max number of digits
 typedef unsigned long UL;
 
-// Verylong - class of very long numbers
+
 class Verylong
 {
 public:
-	Verylong() : m_length(0), m_sign(true) { m_string[0] = '\0'; }
-	Verylong(const char s[SIZE]) 
-	{  
-		strcpy_s(m_string, s); 
-		m_length = std::strlen(s);
-		m_sign = true;
-	}
-	Verylong(const long n)
+	Verylong() : m_length(0), m_sign(true)
 	{
-		// Check sign
-		if(n < 0){
-			m_sign = false;
-		}
-		else {
-			m_sign = true;
-		}
-
-		_itoa_s(abs(n), m_string, 10);	// Convert to string
-		_strrev(m_string);		// Flip her
-		m_length = strlen(m_string); // Find length
+		m_string[0] = L'\0';
 	}
+	Verylong(const wchar_t s[SIZE])   
+	{
+		wcscpy(m_string, s); m_length = wcslen(s); m_sign = true;
+	}
+	Verylong(const long number)         
+	{
+		// Define the sign of the number
+		if (number < 0)
+			m_sign = false;
+		else
+			m_sign = true;
 
-	void put() const;
-	void get();
+		// Convert to string
+		_ltow(abs(number), m_string, 10);  
+		// Flip number in row (the higher digits will become the 
+		// lower ones and vice versa)
+		_wcsrev(m_string);              
+		// Find lingth of number in the string							 
+		m_length = wcslen(m_string);    
+	}
+	void put() const;              
+	void get();          
 
-	// Arithmetic operations of +, -, *, / and % , very long integers
-	Verylong operator+(const Verylong &largeNumber);
-	Verylong operator-(const Verylong &largeNumber);
-	Verylong operator*(const Verylong &largeNumber);
-	Verylong operator/(const Verylong &largeNumber);
-	Verylong operator%(const Verylong &largeNumber);
+	Verylong operator+ (const Verylong); 
+	Verylong operator- (const Verylong); 
+	Verylong operator* (const Verylong); 
+	Verylong operator/ (const Verylong); 
+	Verylong operator% (const Verylong); 
 
 private:
-	char m_string[SIZE];	// Number as a string
-	int m_length;			// Length of Verylong string
-	bool m_sign;			// Sign of number, true = '+', false = '-'
+	// Array (string) for storing a very long integer
+	wchar_t m_string[SIZE]; 
+	int m_length;         
+	// Sign of number,  true '+', false '-'
+	bool m_sign;        
 
-	// The sum of two positive very long integers
-	Verylong addBy(const Verylong &largeNumber) const;
-	// The substraction of two positive very long integers
-	Verylong subtract(const Verylong &largeNumber) const;
-	// The multiplication of two positive very long integers
-	Verylong multiply(const Verylong &largeNumber) const;
-	// The division of two positive very long integers
-	// Flag: 
-	// true - result, 
-	// false - tre remaining of division
-	Verylong divide(const Verylong &largeNumber, bool flag) const;
-	// Multiplying the current number by a digit argument
-	Verylong multiDigit(const int) const;
-	// Multiplying the argument by 10
-	Verylong multi_10(const Verylong &largeNumber) const;
+	Verylong addBy(const Verylong) const;  
+	Verylong subtract(const Verylong) const;
+	Verylong multiply(const Verylong) const;
+	Verylong devide(const Verylong, bool) const;
 
-	// Comparison of modules of numbers, excluding signs
-	// if the current number is greater than the argument, then 1 is returned, if less than 0,
-	// if equal, 2 is returned
-	int compare(const Verylong &largeNumber) const;
+	int compare(const Verylong) const;
+	
+	Verylong multdigit(const int) const;                
+	Verylong mult10(const Verylong) const;  
 };
+
 
 #endif // !_VERYLONG_H_
