b8dbb723228ec90c4b3427c81dc9950495e43575
Sadochok(BISEC) <bisec.world@gmail.com>
2024-09-20 01:13:24 +0300

    Update 13_1exe.cpp

--- RunExercise/RunExercise/13/13_1exe.cpp
+++ RunExercise/RunExercise/13/13_1exe.cpp
@@ -2,46 +2,46 @@
 // and test arithmetical funcsions
 
 #include "13_1.h"
+#include <io.h>
+#include <fcntl.h>
 
-int main13_1()
+
+int main()
 {
-	UL number, position;
-	Verylong factorial = 1;
+	_setmode(_fileno(stdout), _O_U16TEXT);
+	_setmode(_fileno(stdin), _O_U16TEXT);
 
-	std::cout << "Input number: ";
-	std::cin >> number;
+	unsigned long number, j;
+	Verylong factorial = 1;          
 
-	for(position = number; position > 0; position--)
-	{
-		factorial = factorial * position;
-	}
-	std::cout << "Factorial: ";
+	wcout << L"Input number: ";
+	wcin >> number;
+
+	for (j = number; j > 0; j--)
+		factorial = factorial * j;
+	wcout << L"Factorial = ";
 	factorial.put();
-	std::cout << "\n";
+	wcout << endl;
 
-	Verylong result, digit1, digit2;
+	// TESTING
 
-	std::cout << "\nEnter the 1st number: ";
-	digit1.get();
-	std::cout << "\nEnter the 2st number: ";
-	digit2.get();
+	Verylong s, s1, s2;
 
-	result = digit1 + digit2;
-	std::cout << "Sum: "; result.put();
-
-	result = digit1 - digit2;
-	std::cout << "Substruct: "; result.put();
-
-	result = digit1 * digit2;
-	std::cout << "Multiplication: "; result.put();
-
-	result = digit1 / digit2;
-	std::cout << "Division: "; result.put();
-
-	result = digit1 % digit2;
-	std::cout << "The remainder of devision: "; result.put();
-
-	std::cout << "\n";
+	wcout << L"\nEnter the first number: "; s1.get();
+	wcout << L"Enter the second number: "; s2.get();
+	s = s1 + s2;
+	wcout << L"\nSumm: "; s.put();
+	s = s1 - s2;
+	wcout << L"\nSubstruct: "; s.put();
+	s = s1 * s2;
+	wcout << L"\nMultiplication: "; s.put();
+	s = s1 / s2;
+	wcout << L"\nDivision: "; s.put();
+	s = s1 % s2;
+	wcout << L"\nRemainder of the division: "; s.put();
+	wcout << endl;
 
 	return 0;
 }
+
+
